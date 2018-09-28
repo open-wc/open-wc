7aa2dc08fc018f079dcc2ad3373602bc7b7016b9
Thomas Allmer <d4kmor@gmail.com>
2018-09-29 00:59:48 +0200

    fix(storybook): html`` need to work if there are no dynamic parts

--- packages/storybook/index.js
+++ packages/storybook/index.js
@@ -39,6 +39,10 @@ export function html(strings, ...values) {
   const newVal = []; // result values to be passed on to lit-html
   const newStr = []; // result strings to be passed on to lit-html
 
+  if (values.length === 0) {
+    return litHtml(strings, ...values);
+  }
+
   const isDynamicProp = p => p && p.d && typeof p.d === 'string' && Object.keys(p).length === 1;
   const addToCurrentString = (add) => {
     newStr[newStr.length - 1] = newStr[newStr.length - 1] + add;
