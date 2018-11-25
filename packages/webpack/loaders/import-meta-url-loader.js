e02fd321f95fbfd7e2efce48384881fe076963b3
Lars den Bakker <lars.den.bakker@ing.nl>
2018-11-25 10:26:21 +0100

    feat(webpack): added import.meta.url loader

---
+++ packages/webpack/loaders/import-meta-url-loader.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+const regex = /(?!\()import.meta.url(?=\))/g;
+
+/** Webpack loader to rewrite `import.meta.url` in modules to the source code file location. */
+module.exports = function (source) {
+  const relativePath = this.context.substring(
+    this.context.indexOf(this.rootContext) + this.rootContext.length,
+    this.resource.lastIndexOf('/') + 1
+  );
+
+  const fileName = this.resource.substring(this.resource.lastIndexOf('/') + 1);
+  return source.replace(regex, () => `'./${relativePath}/${fileName}'`);
+};
