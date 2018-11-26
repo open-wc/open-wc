ba82eccd5fc96e76e348b3fea213e890d938113a
Lars den Bakker <larsdenbakker@gmail.com>
2018-11-26 18:31:23 +0100

    feat(testing-karma): provide latest and es5 karma config creators

---
+++ packages/testing-karma/create-karma-config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+
+/**
+ * Creates a basic karma configuration file.
+ *
+ * See demo/karma.conf.js for an example implementation.
+ */
+module.exports = config => ({
+  browsers: [
+    'ChromeHeadlessNoSandbox',
+  ],
+
+  customLaunchers: {
+    ChromeHeadlessNoSandbox: {
+      base: 'ChromeHeadless',
+      flags: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ],
+    },
+  },
+
+  frameworks: ['mocha'],
+
+  middleware: ['static'],
+
+  static: {
+    path: path.join(__dirname, ''),
+  },
+
+  preprocessors: {
+    '**/*.test.js': ['webpack', 'sourcemap'],
+    '**/*.spec.js': ['webpack', 'sourcemap'],
+  },
+
+  webpackMiddleware: {
+    stats: 'errors-only',
+  },
+
+  reporters: ['mocha', 'coverage-istanbul'],
+
+  mochaReporter: {
+    showDiff: true,
+  },
+
+  colors: true,
+
+  // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+  logLevel: config.LOG_INFO,
+
+  // ## code coverage config
+  coverageIstanbulReporter: {
+    reports: ['html', 'lcovonly', 'text-summary'],
+    dir: 'coverage',
+    combineBrowserReports: true,
+    skipFilesWithNoCoverage: true,
+    thresholds: {
+      global: {
+        statements: 90,
+        branches: 90,
+        functions: 90,
+        lines: 90,
+      },
+    },
+  },
+
+  webpack: {
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'istanbul-instrumenter-loader',
+          enforce: 'post',
+          include: path.resolve('./'),
+          exclude: /node_modules|bower_components|\.test\.js$/,
+          options: {
+            esModules: true,
+          },
+        },
+
+        {
+          test: /\.js$/,
+          loader: require.resolve('@open-wc/webpack/loaders/import-meta-url-loader.js'),
+        },
+      ],
+    },
+  },
+
+  autoWatch: false,
+  singleRun: true,
+  concurrency: Infinity,
+});
---
+++ packages/testing-karma/demo/src/example.js
@@ -0,0 +1,6 @@
+export function foo(bar) {
+  if (bar) {
+    return false;
+  }
+  return true;
+}
