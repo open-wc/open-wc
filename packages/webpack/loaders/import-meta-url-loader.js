b5d201001c3721a7ab928703aa9fcc758c46c2bc
Thomas Allmer <d4kmor@gmail.com>
2018-12-01 02:09:46 +0100

    fix(webpack): remove extra "/" in generated url in meta-url-loader

--- packages/webpack/loaders/import-meta-url-loader.js
+++ packages/webpack/loaders/import-meta-url-loader.js
@@ -4,7 +4,7 @@ const regex = /(?!\()import.meta.url(?=\))/g;
 /** Webpack loader to rewrite `import.meta.url` in modules to the source code file location. */
 module.exports = function (source) {
   const relativePath = this.context.substring(
-    this.context.indexOf(this.rootContext) + this.rootContext.length,
+    this.context.indexOf(this.rootContext) + this.rootContext.length + 1,
     this.resource.lastIndexOf('/') + 1
   );
 
