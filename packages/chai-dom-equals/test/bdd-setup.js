6f6c2fd013aed2ac0703ee70068c9303ca11ef69
wangpan <doria.panwang@gmail.com>
2018-12-04 22:41:07 -0500

    issue-71

--- packages/chai-dom-equals/test/bdd-setup.js
+++ packages/chai-dom-equals/test/bdd-setup.js
@@ -4,16 +4,19 @@ import { cachedWrappers } from '@open-wc/testing-helpers/fixture.js';
 import { chaiDomEquals } from '../chai-dom-equals.js';
 
 // register-cleanup
-if (afterEach) {
-  afterEach(() => {
-    if (cachedWrappers) {
-      cachedWrappers.forEach((wrapper) => {
-        document.body.removeChild(wrapper);
-      });
-    }
-    cachedWrappers.length = 0; // reset it like this as we can't reassign it
-  });
-}
+try{
+  if (afterEach) {
+    afterEach(() => {
+      if (cachedWrappers) {
+        cachedWrappers.forEach((wrapper) => {
+          document.body.removeChild(wrapper);
+        });
+      }
+      cachedWrappers.length = 0; // reset it like this as we can't reassign it
+    });
+  }
+}catch(error){}
+
 
 // register-plugins
 chai.use(chaiDomEquals);
--- packages/testing/register-fixture-cleanup.js
+++ packages/testing/register-fixture-cleanup.js
@@ -1,12 +1,16 @@
 import { cachedWrappers } from '@open-wc/testing-helpers/fixture.js';
 
-if (afterEach) {
-  afterEach(() => {
-    if (cachedWrappers) {
-      cachedWrappers.forEach((wrapper) => {
-        document.body.removeChild(wrapper);
-      });
-    }
-    cachedWrappers.length = 0; // reset it like this as we can't reassign it
-  });
+try{
+  if (afterEach) {
+      afterEach(() => {
+      if (cachedWrappers) {
+        cachedWrappers.forEach((wrapper) => {
+          document.body.removeChild(wrapper);
+        });
+      }
+      cachedWrappers.length = 0; // reset it like this as we can't reassign it
+    });
+  }
 }
+catch(error){}
+
