99c8f7761c370ae2ebb50fc5b68c60111d2af8d5
Thomas Allmer <d4kmor@gmail.com>
2019-01-19 23:12:50 +0100

    fix: move fixtureCleanup to testing helpers

--- packages/testing/test/side-effects.test.js
+++ packages/testing/test/side-effects.test.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-expressions */
 
-import { cachedWrappers } from '@open-wc/testing-helpers/fixtureWrapper.js';
 import { fixture, expect } from '../index.js';
 
 describe('BDD', () => {
@@ -9,10 +8,6 @@ describe('BDD', () => {
     const el2 = await fixture('<div class="el2"></div>');
     expect(el).to.not.be.undefined;
     expect(el2).to.not.be.undefined;
-  });
-
-  it('will cleanup after each test', async () => {
-    expect(cachedWrappers.length).to.equal(0);
   });
 
   it('uses chai dom equals plugin', async () => {
