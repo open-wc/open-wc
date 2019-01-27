cb7afe101e6a6ec92e74a15977d15cadaa4f8a43
Thomas Allmer <d4kmor@gmail.com>
2019-01-27 17:38:10 +0100

    docs: add minimal meta info

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -99,4 +99,23 @@ module.exports = {
   dest: '_site',
   plugins: ['@vuepress/google-analytics'],
   ga: 'UA-131782693-1',
+  head: [
+    [
+      'meta',
+      {
+        property: 'og:image',
+        content: 'https://github.com/open-wc/open-wc/raw/master/assets/images/logo.png',
+      },
+    ],
+    ['meta', { property: 'og:site_name', content: 'Open Web Components' }],
+    [
+      'meta',
+      {
+        property: 'og:description',
+        content:
+          'Open Web Components provides a set of defaults, recommendations and tools to help facilitate your web component project. Our recommendations include: developing, linting, testing, building, tooling, demoing, publishing and automating.',
+      },
+    ],
+    ['meta', { name: 'twitter:card', content: 'summary' }],
+  ],
 };
