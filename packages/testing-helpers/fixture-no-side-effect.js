fa8573c573d4e4c22490841f6a196ebbc799dd61
Vlad Rindevich <rindevich.vs@gmail.com>
2019-02-09 20:49:46 +0300

    fix(testing-helpers): use asynchronous fixtures

--- packages/testing-helpers/fixture-no-side-effect.js
+++ packages/testing-helpers/fixture-no-side-effect.js
@@ -1,6 +1,6 @@
 import { TemplateResult } from 'lit-html';
-import { stringFixtureSync } from './stringFixture.js';
-import { litFixtureSync } from './litFixture.js';
+import { stringFixture, stringFixtureSync } from './stringFixture.js';
+import { litFixture, litFixtureSync } from './litFixture.js';
 
 /**
  * Setups an element synchronously from the provided string template and puts it in the DOM.
@@ -28,10 +28,10 @@ export function fixtureSync(template) {
  */
 export async function fixture(template) {
   if (typeof template === 'string') {
-    return stringFixtureSync(template);
+    return stringFixture(template);
   }
   if (template instanceof TemplateResult) {
-    return litFixtureSync(template);
+    return litFixture(template);
   }
   throw new Error('Invalid template provided - string or lit-html TemplateResult is supported');
 }
