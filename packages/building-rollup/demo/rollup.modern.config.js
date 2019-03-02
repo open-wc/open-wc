15cc43c1712c4ef3159f584c00b962afa7ffdf0e
Lars den Bakker <larsdenbakker@gmail.com>
2019-03-02 22:12:06 +0100

    fix: add copy to modern build

--- packages/building-rollup/demo/rollup.modern.config.js
+++ packages/building-rollup/demo/rollup.modern.config.js
@@ -1,3 +1,22 @@
+import copy from 'rollup-plugin-copier';
 import createDefaultConfig from '../modern-config';
 
-export default createDefaultConfig({ input: './demo/index.html' });
+const config = createDefaultConfig({
+  input: './demo/index.html',
+});
+
+export default {
+  ...config,
+  plugins: [
+    ...config.plugins,
+    copy({
+      items: [
+        {
+          src: 'demo/a/b/foo.txt',
+          dest: 'dist/demo/a/b/foo.txt',
+          createPath: true,
+        },
+      ],
+    }),
+  ],
+};
