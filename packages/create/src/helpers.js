55af02eec1d4992f5a4a7e6eef402d0eefb3bd8d
Pascal Schilp <pascalschilp@gmail.com>
2019-03-10 17:56:07 +0100

    fix: add cli options (#276)
    * feat: add cli options
    
    * chore: move import
    
    * chore: fix prettier
    
    * feat: improve clioptions
    
    * feat: add question support for npm install
    
    * chore: rename _installNpm
    
    * chore: use getClassName function

--- packages/create/src/helpers.js
+++ packages/create/src/helpers.js
@@ -1,4 +1,5 @@
 import qoa from 'qoa';
+import { cliOptions } from './Generator.js';
 
 function getClassName(tagName) {
   return tagName
@@ -6,22 +7,38 @@ function getClassName(tagName) {
     .reduce((previous, part) => previous + part.charAt(0).toUpperCase() + part.slice(1), '');
 }
 
+export function parseCliOptions(optns) {
+  Object.keys(optns).forEach(key => {
+    if (optns[key] === 'false') optns[key] = false; // eslint-disable-line no-param-reassign
+    if (optns[key] === 'true') optns[key] = true; // eslint-disable-line no-param-reassign
+  });
+  return optns;
+}
+
 export async function askTagInfo() {
   // before super to also affect the Mixin it applies
   let tagName = '';
-  do {
-    // eslint-disable-next-line no-await-in-loop
-    const result = await qoa.prompt([
-      {
-        type: 'input',
-        query: 'Give it a tag name (min two words separated by dashes)',
-        handle: 'tagName',
-      },
-    ]);
-    // eslint-disable-next-line prefer-destructuring
-    tagName = result.tagName;
-  } while (/^([a-z])(?!.*[<>])(?=.*-).+$/.test(tagName) === false);
-  const className = getClassName(tagName);
+  let className = '';
+
+  if (!cliOptions['tag-name']) {
+    tagName = '';
+    do {
+      // eslint-disable-next-line no-await-in-loop
+      const result = await qoa.prompt([
+        {
+          type: 'input',
+          query: 'Give it a tag name (min two words separated by dashes)',
+          handle: 'tagName',
+        },
+      ]);
+      // eslint-disable-next-line prefer-destructuring
+      tagName = result.tagName;
+    } while (/^([a-z])(?!.*[<>])(?=.*-).+$/.test(tagName) === false);
+    className = getClassName(tagName);
+  } else {
+    tagName = cliOptions['tag-name'];
+    className = getClassName(cliOptions['tag-name']);
+  }
 
   return { className, tagName };
 }
