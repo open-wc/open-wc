86d4fa538b4e0ba14ac448006b6496c0f43639b4
thepassle <pascalschilp@gmail.com>
2019-03-19 22:25:40 +0100

    fix: linting

--- packages/create/src/generators/starter-app/templates/static/.eslintrc.js
+++ packages/create/src/generators/starter-app/templates/static/.eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['@open-wc/eslint-config', 'eslint-config-prettier', 'plugin:lit/recommended'].map(require.resolve),
+  extends: ['@open-wc/eslint-config', 'eslint-config-prettier', 'plugin:lit/recommended']
 };
