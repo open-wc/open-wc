157e7eee5730149a1a69e3962f6b3341bcb1ca72
thepassle <pascalschilp@gmail.com>
2019-03-19 19:49:16 +0100

    fix: extend lit plugin

--- packages/create/src/generators/starter-app/templates/static/.eslintrc.js
+++ packages/create/src/generators/starter-app/templates/static/.eslintrc.js
@@ -1,15 +1,3 @@
 module.exports = {
-  extends: ['@open-wc/eslint-config', 'eslint-config-prettier'].map(require.resolve),
-  plugins: ['lit'],
-  rules: {
-    'lit/no-duplicate-template-bindings': 'error',
-    'lit/no-legacy-template-syntax': 'error',
-    'lit/no-template-bind': 'error',
-    'lit/no-template-map': 'error',
-    'lit/no-useless-template-literals': 'error',
-    'lit/attribute-value-entities': 'error',
-    'lit/binding-positions': 'error',
-    'lit/no-property-change-update': 'error',
-    'lit/no-invalid-html': 'error',
-  },
+  extends: ['@open-wc/eslint-config', 'eslint-config-prettier', 'plugin:lit/recommended'].map(require.resolve),
 };
