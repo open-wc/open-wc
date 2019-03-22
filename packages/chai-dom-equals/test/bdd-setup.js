7b454bbceaf4b7f9fc61e9bc53d9b117283c3504
Thomas Allmer <d4kmor@gmail.com>
2019-03-23 00:20:48 +0100

    chore: publish only needed files

--- packages/chai-dom-equals/test/bdd-setup.js
+++ packages/chai-dom-equals/test/bdd-setup.js
@@ -1,6 +1,6 @@
 // do manual setup and not use testing to not have circle dependencies
 import { chai } from '@bundled-es-modules/chai';
-import { cachedWrappers } from '@open-wc/testing-helpers/fixtureWrapper.js';
+import { cachedWrappers } from '@open-wc/testing-helpers/index-no-side-effects.js';
 import { chaiDomEquals } from '../chai-dom-equals.js';
 
 // register-cleanup
