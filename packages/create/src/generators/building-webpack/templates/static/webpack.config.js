d0dc479e5cd1372316ac3bf2835cd5575ec35857
thepassle <pascalschilp@gmail.com>
2019-03-26 18:25:00 +0100

    fix: default webpack generator config and docs

--- packages/create/src/generators/building-webpack/templates/static/webpack.config.js
+++ packages/create/src/generators/building-webpack/templates/static/webpack.config.js
@@ -1,3 +1,10 @@
-const defaultConfig = require('@open-wc/building-webpack/default-config');
+const { resolve } = require('path');
+const createDefaultConfig = require('@open-wc/building-webpack/modern-and-legacy-config');
 
-module.exports = defaultConfig();
+// If you don't need IE11 support, use the modern-config instead
+// import createDefaultConfig from '@open-wc/building-webpack/modern-config';
+
+module.exports = createDefaultConfig({
+  entry: resolve(__dirname, './src/index.js'),
+  indexHTML: resolve(__dirname, './src/index.html'),
+});
