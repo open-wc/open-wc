38d65446cc5c2b49208da83c1fbe3a508154619e
kasperstorgaard <kasper.storgaard@gmail.com>
2019-06-10 13:26:59 +0200

    fix(building-rollup): add extensions option - for babel and node-resolve, to properly support .ts files

--- packages/building-rollup/demo/ts-babel/rollup.config.js
+++ packages/building-rollup/demo/ts-babel/rollup.config.js
@@ -2,4 +2,5 @@ const createDefaultConfig = require('../../modern-and-legacy-config');
 
 module.exports = createDefaultConfig({
   input: './demo/ts-babel/index.html',
+  extensions: ['.js', '.ts'],
 });
