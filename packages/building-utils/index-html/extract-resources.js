1876e51da675d5cc30fa2d0efe8e7a590227081f
Thomas Allmer <d4kmor@gmail.com>
2019-06-29 21:24:27 +0200

    feat(rollup-plugin-index-html): support import maps

--- packages/building-utils/index-html/extract-resources.js
+++ packages/building-utils/index-html/extract-resources.js
@@ -5,6 +5,8 @@ const { queryAll, predicates, getAttribute, remove } = require('../dom5-fork');
  * @typedef {object} ExtractResult
  * @property {import('parse5').ASTNode} indexHTML the index file, with resources removed
  * @property {string[]} jsModules paths to js modules that were found
+ * @property {string[]} inlineImportMaps content of inline import maps that were found
+ * @property {string[]} importMapPaths paths to import map json files that were found
  */
 
 /**
@@ -21,7 +23,11 @@ function extractResources(htmlString) {
 
   const scripts = queryAll(indexHTML, predicates.hasTagName('script'));
   const moduleScripts = scripts.filter(script => getAttribute(script, 'type') === 'module');
+  const importMapScripts = scripts.filter(script => getAttribute(script, 'type') === 'importmap');
+
   const jsModules = [];
+  const inlineImportMaps = [];
+  const importMapPaths = [];
 
   moduleScripts.forEach(moduleScript => {
     const src = getAttribute(moduleScript, 'src');
@@ -31,8 +37,18 @@ function extractResources(htmlString) {
     }
   });
 
+  importMapScripts.forEach(importMapScript => {
+    const src = getAttribute(importMapScript, 'src');
+    if (!src) {
+      inlineImportMaps.push(importMapScript.childNodes[0].value);
+    } else {
+      importMapPaths.push(src);
+    }
+    remove(importMapScript);
+  });
+
   // TODO: Also extract other kinds of resources
-  return { indexHTML, jsModules };
+  return { indexHTML, jsModules, inlineImportMaps, importMapPaths };
 }
 
 module.exports.extractResources = extractResources;
