a3d863346f85034ede38d60fed5ee556dbe24e4c
thepassle <pascalschilp@gmail.com>
2019-07-14 21:45:33 +0200

    feat(create): move to a single app generator

--- packages/create/src/generators/testing/templates/_my-el.test.js
+++ packages/create/src/generators/testing/templates/_my-el.test.js
@@ -3,17 +3,17 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../src/<%= tagName %>.js';
 
 describe('<<%= tagName %>>', () => {
-  it('has a default property heading', async () => {
+  it('has a default property title', async () => {
     const el = await fixture('<<%= tagName %>></<%= tagName %>>');
 
-    expect(el.heading).to.equal('Hello world!');
+    expect(el.title).to.equal('Hello world!');
   });
 
-  it('allows property heading to be overwritten', async () => {
+  it('allows property title to be overwritten', async () => {
     const el = await fixture(html`
-      <<%= tagName %> heading="different heading"></<%= tagName %>>
+      <<%= tagName %> title="different title"></<%= tagName %>>
     `);
 
-    expect(el.heading).to.equal('different heading');
+    expect(el.title).to.equal('different title');
   });
 });
