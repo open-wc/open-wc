add4f6ea1e02568245eaf7363cfa836221414cf4
Thomas Allmer <d4kmor@gmail.com>
2019-07-16 01:31:59 +0200

    fix(es-dev-server): export polyfill configs via constants.js

--- packages/es-dev-server/src/constants.js
+++ packages/es-dev-server/src/constants.js
@@ -1,5 +1,6 @@
 export const compatibilityModes = {
   NONE: 'none',
+  ESM: 'esm',
   MODERN: 'modern',
   ALL: 'all',
 };
@@ -7,3 +8,18 @@ export const compatibilityModes = {
 export const messageChannelEndpoint = '/__es-dev-server-message-channel__';
 
 export const baseFileExtensions = ['.js', '.mjs'];
+
+/** @type {import('@open-wc/building-utils/index-html/create-index-html').PolyfillsConfig} */
+export const modernPolyfills = {
+  webcomponents: true,
+  esModuleShims: true,
+};
+
+/** @type {import('@open-wc/building-utils/index-html/create-index-html').PolyfillsConfig} */
+export const legacyPolyfills = {
+  ...modernPolyfills,
+  coreJs: true,
+  regeneratorRuntime: true,
+  fetch: true,
+  systemJsExtended: true,
+};
--- packages/es-dev-server/src/middleware/compatibility.js
+++ packages/es-dev-server/src/middleware/compatibility.js
@@ -1,7 +1,7 @@
 import { extractResources, createIndexHTML } from '@open-wc/building-utils/index-html/index.js';
 import path from 'path';
 import { getBodyAsString } from '../utils.js';
-import { compatibilityModes } from '../constants.js';
+import { compatibilityModes, modernPolyfills, legacyPolyfills } from '../constants.js';
 import systemJsLegacyResolveScript from '../browser-scripts/system-js-legacy-resolve.js';
 
 /**
@@ -10,21 +10,6 @@ import systemJsLegacyResolveScript from '../browser-scripts/system-js-legacy-res
  * @property {string} appIndexDir
  * @property {string} compatibilityMode
  */
-
-/** @type {import('@open-wc/building-utils/index-html/create-index-html').PolyfillsConfig} */
-const modernPolyfills = {
-  webcomponents: true,
-  esModuleShims: true,
-};
-
-/** @type {import('@open-wc/building-utils/index-html/create-index-html').PolyfillsConfig} */
-const legacyPolyfills = {
-  ...modernPolyfills,
-  coreJs: true,
-  regeneratorRuntime: true,
-  fetch: true,
-  systemJsExtended: true,
-};
 
 function injectImportMaps(indexHTML, resources, type) {
   let transformedIndexHTML = indexHTML;
@@ -79,7 +64,8 @@ export function createCompatibilityMiddleware(cfg) {
     if (polyfill) {
       ctx.body = polyfill;
       // aggresively cache polyfills, they are hashed so content bust the cache
-      ctx.response.append('Cache-Control', 'public, max-age=31536000');
+      ctx.response.set('cache-control', 'public, max-age=31536000');
+      ctx.response.set('content-type', 'text/javascript');
       return;
     }
 
