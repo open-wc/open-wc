00457b5209c47f8bee116285aecc3cb37402b75f
Westbrook Johnson <westbrook.johnson@gmail.com>
2019-07-22 21:35:41 -0400

    feat(building-webpack): add main entry point

---
+++ packages/building-webpack/index.js
@@ -0,0 +1,4 @@
+const createConfig = require('./modern-config.js');
+const createCompatibilityConfig = require('./modern-and-legacy-config.js');
+
+module.exports = { createConfig, createCompatibilityConfig };
