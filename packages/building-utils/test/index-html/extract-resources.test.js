95988cf6e35c677d728c816a16a5435587a797f6
Lars den Bakker <larsdenbakker@gmail.com>
2019-07-23 19:56:02 +0200

    fix(building-utils): don't extract external resources

--- packages/building-utils/test/index-html/extract-resources.test.js
+++ packages/building-utils/test/index-html/extract-resources.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 const { extractResources } = require('../../index-html');
-const { queryAll, predicates } = require('../../dom5-fork');
+const { getAttribute, queryAll, predicates } = require('../../dom5-fork');
 
 describe('extract-resources', () => {
   it('returns any resources found', () => {
@@ -37,5 +37,28 @@ describe('extract-resources', () => {
 
     const result = extractResources(html);
     expect(queryAll(result.indexHTML, predicates.hasTagName('script')).length).to.equal(0);
+  });
+
+  it('does not touch any resources which reference external sources', () => {
+    const html = `
+      <html>
+        <head>
+          <script type="module" src="file-a.js"></script>
+        </head>
+        <body>
+          <script src="https://www.foo.com/my-script.js"></script>
+          <script src="http://localhost:123/bar.js"></script>
+          <script type="module" src="file-b.js"></script>
+          <script type="module"></script>
+          <script type="module" src="file-c.js"></script>
+        </body>
+      </html>
+    `;
+
+    const result = extractResources(html);
+    const scripts = queryAll(result.indexHTML, predicates.hasTagName('script'));
+    expect(scripts.length).to.equal(2);
+    expect(getAttribute(scripts[0], 'src')).to.equal('https://www.foo.com/my-script.js');
+    expect(getAttribute(scripts[1], 'src')).to.equal('http://localhost:123/bar.js');
   });
 });
