cfa2793d696db54078aa780fdd9369226c427492
Thomas Allmer <d4kmor@gmail.com>
2019-08-04 14:04:59 +0200

    chore: remove deprecated polyfills-loader package

--- packages/polyfills-loader/src/webcomponents-all.js
+++
@@ -1,5 +0,0 @@
-import '@webcomponents/webcomponents-platform/webcomponents-platform.js';
-import '@webcomponents/template/template.js';
-import '@webcomponents/shadydom/src/shadydom.js';
-import '@webcomponents/custom-elements/src/custom-elements.js';
-import '@webcomponents/shadycss/entrypoints/scoping-shim.js';
