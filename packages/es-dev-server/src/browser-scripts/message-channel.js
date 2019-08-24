194527aabf915a041b8979413e81c52aeef02687
Lars den Bakker <larsdenbakker@gmail.com>
2019-08-24 11:58:16 +0200

    feat(es-dev-server): log compile errors during page load

--- packages/es-dev-server/src/browser-scripts/message-channel.js
+++ packages/es-dev-server/src/browser-scripts/message-channel.js
@@ -10,13 +10,28 @@ export default `
       }
 
       var eventSource = new EventSource('${messageChannelEndpoint}');
+      var reloading = false;
 
       eventSource.addEventListener('file-changed', function (e) {
+        reloading = true;
         location.reload();
       });
 
       eventSource.addEventListener('error-message', function (e) {
+        if (reloading) {
+          return;
+        }
+        
         console.error(JSON.parse(e.data));
+      });
+
+      eventSource.addEventListener('error', function () {
+        if (reloading) {
+          return;
+        }
+
+        console.log('Disconnected from es-dev-server, no longer reloading on file changes.');
+        eventSource.close();
       });
     }());
   </script>
