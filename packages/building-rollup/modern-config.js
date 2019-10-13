55071442edf99a10bbae422c045d5309223a8952
Pascal Schilp <pascalschilp@gmail.com>
2019-10-13 10:58:26 +0200

    feat(building-rollup): add workbox (#845)

--- packages/building-rollup/modern-config.js
+++ packages/building-rollup/modern-config.js
@@ -7,6 +7,8 @@ const resolve = require('rollup-plugin-node-resolve');
 const { terser } = require('rollup-plugin-terser');
 const babel = require('rollup-plugin-babel');
 const indexHTML = require('rollup-plugin-index-html');
+const workbox = require('rollup-plugin-workbox');
+const path = require('path');
 const entrypointHashmanifest = require('rollup-plugin-entrypoint-hashmanifest');
 
 const production = !process.env.ROLLUP_WATCH;
@@ -22,6 +24,12 @@ module.exports = function createBasicConfig(_options) {
     outputDir: 'dist',
     extensions: DEFAULT_EXTENSIONS,
     indexHTMLPlugin: {},
+    plugins: {
+      indexHTML: true,
+      workbox: true,
+      babel: true,
+      ...(_options.plugins || {}),
+    },
     ..._options,
   };
 
@@ -38,14 +46,15 @@ module.exports = function createBasicConfig(_options) {
     },
     plugins: [
       // parse input index.html as input and feed any modules found to rollup
-      indexHTML({
-        ...(options.indexHTMLPlugin || {}),
-        polyfills: {
-          ...((options.indexHTMLPlugin && options.indexHTMLPlugin.polyfills) || {}),
-          dynamicImport: true,
-          webcomponents: true,
-        },
-      }),
+      options.plugins.indexHTML &&
+        indexHTML({
+          ...(options.indexHTMLPlugin || {}),
+          polyfills: {
+            ...((options.indexHTMLPlugin && options.indexHTMLPlugin.polyfills) || {}),
+            dynamicImport: true,
+            webcomponents: true,
+          },
+        }),
 
       // resolve bare import specifiers
       resolve({
@@ -53,51 +62,68 @@ module.exports = function createBasicConfig(_options) {
       }),
 
       // run code through babel
-      babel({
-        extensions: options.extensions,
-        plugins: [
-          '@babel/plugin-syntax-dynamic-import',
-          '@babel/plugin-syntax-import-meta',
-          // rollup rewrites import.meta.url, but makes them point to the file location after bundling
-          // we want the location before bundling
-          'bundled-import-meta',
-          production && [
-            'template-html-minifier',
-            {
-              modules: {
-                'lit-html': ['html'],
-                'lit-element': ['html', { name: 'css', encapsulation: 'style' }],
+      options.plugins.babel &&
+        babel({
+          extensions: options.extensions,
+          plugins: [
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-syntax-import-meta',
+            // rollup rewrites import.meta.url, but makes them point to the file location after bundling
+            // we want the location before bundling
+            'bundled-import-meta',
+            production && [
+              'template-html-minifier',
+              {
+                modules: {
+                  'lit-html': ['html'],
+                  'lit-element': ['html', { name: 'css', encapsulation: 'style' }],
+                },
+                htmlMinifier: {
+                  collapseWhitespace: true,
+                  removeComments: true,
+                  caseSensitive: true,
+                  minifyCSS: customMinifyCss,
+                },
               },
-              htmlMinifier: {
-                collapseWhitespace: true,
-                removeComments: true,
-                caseSensitive: true,
-                minifyCSS: customMinifyCss,
-              },
-            },
-          ],
-        ].filter(_ => !!_),
+            ],
+          ].filter(_ => !!_),
 
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              targets: findSupportedBrowsers(),
-              // preset-env compiles template literals for safari 12 due to a small bug which
-              // doesn't affect most use cases. for example lit-html handles it: (https://github.com/Polymer/lit-html/issues/575)
-              exclude: ['@babel/plugin-transform-template-literals'],
-              useBuiltIns: false,
-              modules: false,
-            },
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: findSupportedBrowsers(),
+                // preset-env compiles template literals for safari 12 due to a small bug which
+                // doesn't affect most use cases. for example lit-html handles it: (https://github.com/Polymer/lit-html/issues/575)
+                exclude: ['@babel/plugin-transform-template-literals'],
+                useBuiltIns: false,
+                modules: false,
+              },
+            ],
           ],
-        ],
-      }),
+        }),
 
       // only minify if in production
       production && terser(),
 
       // hash
       entrypointHashmanifest(),
+
+      production &&
+        options.plugins.workbox &&
+        workbox({
+          mode: 'generateSW',
+          workboxConfig: {
+            // for spa client side routing, always return index.html
+            navigateFallback: '/index.html',
+            // where to output the generated sw
+            swDest: path.join(process.cwd(), 'dist', 'sw.js'),
+            // directory to match patterns against to be precached
+            globDirectory: path.join(process.cwd(), 'dist'),
+            // cache any html js and css by default
+            globPatterns: ['**/*.{html,js,css}'],
+          },
+        }),
     ],
   };
 };
