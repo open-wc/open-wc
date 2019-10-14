4675b879062ae9b124f3f5de948a4623db932c08
Passle <pascalschilp@gmail.com>
2019-10-14 22:30:02 +0200

    chore: remove max-width

--- packages/create/src/generators/app-haunted/templates/_MyApp.js
+++ packages/create/src/generators/app-haunted/templates/_MyApp.js
@@ -41,7 +41,6 @@ function styles() {
         justify-content: flex-start;
         font-size: calc(10px + 2vmin);
         color: #1a2b42;
-        max-width: 960px;
         margin: 0 auto;
       }
       :host > header,
