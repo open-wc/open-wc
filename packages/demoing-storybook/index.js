94cf36e8b349991dd2e149cd35a25530df9e1b27
Joren Broekema <joren.broekema@ing.com>
2019-10-15 16:31:49 +0200

    chore(demoing-storybook): export withA11y decorator

--- packages/demoing-storybook/index.js
+++ packages/demoing-storybook/index.js
@@ -1,7 +1,8 @@
 export { html } from 'lit-html';
 
-export { storiesOf, addParameters } from '@storybook/polymer';
+export { storiesOf, addParameters, addDecorator } from '@storybook/polymer';
 export { action } from '@storybook/addon-actions';
+export { withA11y } from '@storybook/addon-a11y';
 export { linkTo } from '@storybook/addon-links';
 export { withNotes } from '@storybook/addon-notes';
 export { document } from 'global';
