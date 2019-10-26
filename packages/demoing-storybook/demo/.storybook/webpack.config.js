20897a585f3d460f2c467f169366b8f202744d8d
Thomas Allmer <d4kmor@gmail.com>
2019-10-26 15:06:47 +0200

    feat(demoing-storybook): storybook 5.3 with docs mode - see MIGRATION.md

--- packages/demoing-storybook/demo/.storybook/webpack.config.js
+++
@@ -1,5 +0,0 @@
-const defaultConfig = require('../../default-storybook-webpack-config.js');
-
-module.exports = ({ config }) => {
-  return defaultConfig({ config, transpilePackages: ['lit-html', 'lit-element', '@open-wc'] });
-};
