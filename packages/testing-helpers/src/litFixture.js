8672ab665124ee905838212c2eaeab612f67f171
bennypowers <benny.powers@forter.com>
2019-10-29 08:07:23 +0200

    feat(testing-helpers): allow rendering non-TemplateResult
    `render` from `lit-html` accepts more than just a `TemplateResult` 
    nowadays.
    testing-helpers can support string, number, boolean, TemplateResult,
    Node, or Arrays or iterables of such.
    
    Note: in the case of Array or Iterable, only the first child will be
    taken. All others will be silently ignored. See 
    https://github.com/open-wc/open-wc/issues/833
    
    affects: @open-wc/testing-helpers

--- packages/testing-helpers/src/litFixture.js
+++ packages/testing-helpers/src/litFixture.js
@@ -1,25 +1,53 @@
+import { TemplateResult } from 'lit-html';
 import { fixtureWrapper } from './fixtureWrapper.js';
 import { render } from './lit-html.js';
 import { elementUpdated } from './elementUpdated.js';
+import { NODE_TYPES } from './lib.js';
+
+/**
+ * @typedef {
+     import('lit-html').TemplateResult | import('lit-html').TemplateResult[]
+   | Node | Node[]
+   | string | string[]
+   | number | number[]
+   | boolean | boolean[]
+ } LitHTMLRenderable
+ */
+
+const isUsefulNode = ({ nodeType, textContent }) => {
+  switch (nodeType) {
+    case NODE_TYPES.COMMENT_NODE:
+      return false;
+    case NODE_TYPES.TEXT_NODE:
+      return textContent.trim();
+    default:
+      return true;
+  }
+};
 
 /**
  * Setups an element synchronously from the provided lit-html template and puts it in the DOM.
  *
  * @template {Element} T - Is an element or a node
- * @param {import('lit-html').TemplateResult} template
+ * @param {LitHTMLRenderable} template
  * @returns {T}
  */
 export function litFixtureSync(template) {
   const wrapper = fixtureWrapper();
   render(template, wrapper);
-  return /** @type {T} */ (wrapper.children[0]);
+  if (template instanceof TemplateResult) {
+    return /** @type {T} */ (wrapper.firstElementChild);
+  }
+  const [node] = Array.from(wrapper.childNodes).filter(isUsefulNode);
+
+  return /** @type {T} */ (node);
 }
 
 /**
  * Setups an element asynchronously from the provided lit-html template and puts it in the DOM.
  *
  * @template {Element} T - Is an element or a node
- * @param {import('lit-html').TemplateResult} template
+ * @param {LitHTMLRenderable} template
  * @returns {Promise<T>}
  */
 export async function litFixture(template) {
