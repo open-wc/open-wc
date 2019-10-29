637dfeb32f3cf442d0046b3279556e3f830edf39
bennypowers <benny.powers@forter.com>
2019-10-29 11:52:12 +0200

    feat(create): dynamic READMEs
    closes #917
    
    affects: @open-wc/create

--- packages/create/src/generators/wc-lit-element/index.js
+++ packages/create/src/generators/wc-lit-element/index.js
@@ -1,9 +1,31 @@
+import { join } from 'path';
 import { CommonRepoMixin } from '../common-repo/index.js';
+import { processTemplate, readFileFromPath } from '../../core.js';
+
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
+
+const getTemplatePart = compose(
+  processTemplate,
+  readFileFromPath,
+);
+
+function featureReadmeBlurb(feature) {
+  const path = join(__dirname, `./templates/partials/README.${feature}.md`);
+  return getTemplatePart(path);
+}
+
+function featureReadme(acc, feature, i, a) {
+  return `${acc + featureReadmeBlurb(feature)}${i === a.length - 1 ? '' : '\n'}`;
+}
 
 /* eslint-disable no-console */
 export const WcLitElementMixin = subclass =>
   class extends subclass {
     async execute() {
+      this.templateData.featureReadmes = this.options.features.reduce(featureReadme, '');
+      this.templateData.scriptRunCommand =
+        this.options.installDependencies === 'yarn' ? 'yarn' : 'npm run';
+
       await super.execute();
       const { tagName, className } = this.templateData;
 
