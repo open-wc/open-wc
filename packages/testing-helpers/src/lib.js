15345c7a62ac674205a1134a668fa4b17fa68f49
Benny Powers <bennypowers@users.noreply.github.com>
2019-11-02 18:33:06 +0200

    feat(testing-helpers): allow rendering non-TemplateResult (#910)
    `render` from `lit-html` accepts more than just a `TemplateResult` 
    nowadays.
    testing-helpers can support string, number, boolean, TemplateResult,
    Node, or Arrays or iterables of such.
    
    Note: in the case of Array or Iterable, only the first child will be
    taken. All others will be silently ignored. See 
    https://github.com/open-wc/open-wc/issues/833
    
    affects: @open-wc/testing-helpers

---
+++ packages/testing-helpers/src/lib.js
@@ -0,0 +1,29 @@
+import { TemplateResult } from 'lit-html';
+
+export const isIterable = object => object != null && typeof object[Symbol.iterator] === 'function';
+
+function isValidNonIterableRenderArg(x) {
+  return (
+    x instanceof TemplateResult ||
+    x instanceof Node ||
+    typeof x === 'number' ||
+    typeof x === 'boolean' ||
+    typeof x === 'string'
+  );
+}
+
+export function isValidRenderArg(x) {
+  return isIterable(x) ? [...x].every(isValidNonIterableRenderArg) : isValidNonIterableRenderArg(x);
+}
+
+/**
+ * Node#nodeType enum
+ * @readonly
+ * @enum {number}
+ */
+export const NODE_TYPES = Object.freeze({
+  ELEMENT_NODE: 1,
+  TEXT_NODE: 3,
+  COMMENT_NODE: 8,
+  DOCUMENT_FRAGMENT_NODE: 11,
+});
