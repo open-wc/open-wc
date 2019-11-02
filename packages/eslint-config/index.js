0019c95892ab7915164c43537980c7d960b1ba78
Lars den Bakker <larsdenbakker@gmail.com>
2019-11-02 21:42:12 +0100

    feat(eslint-config): allow for-of loops in eslint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -12,6 +12,25 @@ module.exports = {
   rules: {
     'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
     'no-underscore-dangle': 'off',
+    // air bnb restricts for of loops, which we want to allow. we can't cherry pick it out, so we have to copy over the existing rules
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
     'no-only-tests/no-only-tests': 'error',
     'import/extensions': ['error', 'always', { ignorePackages: true }],
     'import/prefer-default-export': 'off',
