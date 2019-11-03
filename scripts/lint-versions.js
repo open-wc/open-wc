2db41ff803c4b2dd7bd76abf6e752c5ac1630f79
Thomas Allmer <d4kmor@gmail.com>
2019-11-03 17:03:02 +0100

    chore: lint alignment of all versions in the monorepo

---
+++ scripts/lint-versions.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+const { readdirSync, existsSync, readFileSync } = require('fs');
+
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(pathMeta => pathMeta.isDirectory())
+    .map(pathMeta => pathMeta.name);
+
+function readPackageJson(filePath) {
+  if (existsSync(filePath)) {
+    const jsonData = JSON.parse(readFileSync(filePath, 'utf-8'));
+    const merged = { ...jsonData.dependencies, ...jsonData.devDependencies };
+    const result = {};
+    Object.keys(merged).forEach(dep => {
+      if (merged[dep] && !merged[dep].includes('file:')) {
+        result[dep] = merged[dep];
+      }
+    });
+    return result;
+  }
+  return {};
+}
+
+function compareVersions(versionsA, versionsB) {
+  let output = '';
+  Object.keys(versionsA).forEach(dep => {
+    if (versionsA[dep] && versionsB[dep] && versionsA[dep] !== versionsB[dep]) {
+      output += `  - "${dep}": "${versionsA[dep]}" !== "${versionsB[dep]}"`;
+    }
+  });
+  return output;
+}
+
+const currentVersions = readPackageJson('./package.json');
+let endReturn = 0;
+getDirectories('./packages').forEach(subPackage => {
+  const filePath = `./packages/${subPackage}/package.json`;
+  const subPackageVersions = readPackageJson(filePath);
+  const result = compareVersions(currentVersions, subPackageVersions);
+  if (result) {
+    console.log(`Version miss matches found in "${filePath}":`);
+    console.log(result);
+    console.log();
+    endReturn = 1;
+  }
+});
+
+process.exit(endReturn);
