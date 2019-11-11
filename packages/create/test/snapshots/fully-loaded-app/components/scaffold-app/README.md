3e897a81a1833635083955a2a5c12fd76543b1a3
bennypowers <benny.powers@forter.com>
2019-11-11 11:40:21 +0200

    test(create): add basic integration test for app with all features
    fixes: #941
    closes: #970

---
+++ packages/create/test/snapshots/fully-loaded-app/components/scaffold-app/README.md
@@ -0,0 +1,30 @@
+# \<scaffold-app>
+
+This webcomponent follows the [open-wc](https://github.com/open-wc/open-wc) recommendation.
+
+## Installation
+```bash
+npm i scaffold-app
+```
+
+## Usage
+```html
+<script type="module">
+  import 'scaffold-app/scaffold-app.js';
+</script>
+
+<scaffold-app></scaffold-app>
+```
+
+undefined
+
+## Local Demo with `es-dev-server`
+```bash
+npm start
+```
+To run a local development server that serves the basic demo located in `demo/index.html`
+
+```bash
+npm start:compatibility
+```
+To run a local development server in compatibility mode for older browsers that serves the basic demo located in `demo/index.html`
---
+++ packages/create/test/snapshots/fully-loaded-app/components/scaffold-app/src/ScaffoldApp.js
@@ -0,0 +1,144 @@
+import { LitElement, html, css } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { openWcLogo } from './open-wc-logo.js';
+
+import '../../page-main/page-main.js';
+import '../../page-one/page-one.js';
+import { templateAbout } from './templateAbout.js';
+
+export class ScaffoldApp extends LitElement {
+  static get properties() {
+    return {
+      title: { type: String },
+      page: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    this.page = 'main';
+  }
+
+  _renderPage() {
+    switch (this.page) {
+      case 'main':
+        return html`
+          <page-main .logo=${openWcLogo}></page-main>
+        `;
+      case 'pageOne':
+        return html`
+          <page-one></page-one>
+        `;
+      case 'about':
+        return templateAbout;
+      default:
+        return html`
+          <p>Page not found try going to <a href="#main">Main</a></p>
+        `;
+    }
+  }
+
+  __clickPageLink(ev) {
+    ev.preventDefault();
+    this.page = ev.target.hash.substring(1);
+  }
+
+  __addActiveIf(page) {
+    return classMap({ active: this.page === page });
+  }
+
+  render() {
+    return html`
+      <header>
+        <ul>
+          <li>
+            <a href="#main" class=${this.__addActiveIf('main')} @click=${this.__clickPageLink}
+              >Main</a
+            >
+          </li>
+          <li>
+            <a href="#pageOne" class=${this.__addActiveIf('pageOne')} @click=${this.__clickPageLink}
+              >Page One</a
+            >
+          </li>
+          <li>
+            <a href="#about" class=${this.__addActiveIf('about')} @click=${this.__clickPageLink}
+              >About</a
+            >
+          </li>
+        </ul>
+      </header>
+
+      <main>
+        ${this._renderPage()}
+      </main>
+
+      <p class="app-footer">
+        🚽 Made with love by
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/open-wc">open-wc</a>.
+      </p>
+    `;
+  }
+
+  static get styles() {
+    return [
+      css`
+        :host {
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: flex-start;
+          font-size: calc(10px + 2vmin);
+          color: #1a2b42;
+          max-width: 960px;
+          margin: 0 auto;
+        }
+
+        header {
+          width: 100%;
+          background: #fff;
+          border-bottom: 1px solid #ccc;
+        }
+
+        header ul {
+          display: flex;
+          justify-content: space-between;
+          min-width: 400px;
+          margin: 0 auto;
+          padding: 0;
+        }
+
+        header ul li {
+          display: flex;
+        }
+
+        header ul li a {
+          color: #ccc;
+          text-decoration: none;
+          font-size: 18px;
+          line-height: 36px;
+        }
+
+        header ul li a:hover,
+        header ul li a.active {
+          color: #000;
+        }
+
+        main {
+          flex-grow: 1;
+        }
+
+        .app-footer {
+          color: #a8a8a8;
+          font-size: calc(12px + 0.5vmin);
+          align-items: center;
+        }
+
+        .app-footer a {
+          margin-left: 5px;
+        }
+      `,
+    ];
+  }
+}
