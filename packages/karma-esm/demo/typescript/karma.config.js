7d5ea56a1cb911fb31228bec18dc38ea8826b1f4
Lars den Bakker <larsdenbakker@gmail.com>
2019-11-18 19:40:26 +0100

    feat: update testing to use auto compatibility of es-dev-server

--- packages/karma-esm/demo/typescript/karma.config.js
+++ packages/karma-esm/demo/typescript/karma.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 function getCompatibility() {
   const indexOf = process.argv.indexOf('--compatibility');
-  return indexOf === -1 ? 'none' : process.argv[indexOf + 1];
+  return indexOf === -1 ? 'auto' : process.argv[indexOf + 1];
 }
 
 const coverage = process.argv.includes('--coverage');
