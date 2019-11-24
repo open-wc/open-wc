f2d873109b174978ce4da90c76fca81e6d80afbc
Lars den Bakker <larsdenbakker@gmail.com>
2019-11-24 11:48:03 +0100

    chore(building-rollup): add integration tests

--- packages/building-rollup/demo/ts-babel/demo-app.ts
+++ packages/building-rollup/demo/ts-babel/demo-app.ts
@@ -1,11 +1,47 @@
-import { LitElement, html, TemplateResult } from 'lit-element';
+import { LitElement, html, css, customElement } from 'lit-element';
+import './demo-component.ts';
 
+// partial css trips up the minifier
+const fontSize = css`
+  16
+`;
+
+const fontMd = css`
+  font-size: ${fontSize}px;
+`;
+
+@customElement('demo-app')
 class DemoApp extends LitElement {
-  render(): TemplateResult {
+  static styles = css`
+    :host {
+      display: block;
+      color: black;
+      background-color: white;
+      ${fontMd}
+    }
+  `;
+
+  render() {
     return html`
-      <h1>Hello typescript!</h1>
+      <p>Demo app</p>
+      <demo-component></demo-component>
+      <lazy-component></lazy-component>
     `;
   }
 }
 
-customElements.define('demo-app', DemoApp);
+const cssText = DemoApp.styles.cssText.replace(/\s/g, '');
+
+window.__partialCSS = cssText.includes('font-size:16px') && cssText.includes('display:block');
+window.__litElement = (async () => {
+  await import('./lazy-component.ts');
+  const app = document.body.querySelector('demo-app');
+  const demoComponent = app.shadowRoot.querySelector('demo-component');
+  const lazyComponent = app.shadowRoot.querySelector('lazy-component');
+
+  return (
+    app.shadowRoot.innerHTML.includes('<p>Demo app</p>') &&
+    demoComponent.shadowRoot.innerHTML.includes('<p>Demo component</p>') &&
+    lazyComponent.shadowRoot.innerHTML.includes('<p>Lazy component</p>')
+  );
+})();
---
+++ packages/building-rollup/test/integration.test.js
@@ -0,0 +1,90 @@
+/* eslint-disable global-require, import/no-dynamic-require */
+const puppeteer = require('puppeteer');
+const { expect } = require('chai');
+const path = require('path');
+const fs = require('fs');
+const rimraf = require('rimraf');
+const { rollup } = require('rollup');
+const { startServer, createConfig } = require('es-dev-server');
+
+const rootDir = path.resolve(__dirname, '..', 'dist');
+const testCases = ['js', 'ts-babel'];
+
+describe('integration tests', () => {
+  let server;
+  let serverConfig;
+  /** @type {import('puppeteer').Browser} */
+  let browser;
+
+  before(async () => {
+    serverConfig = createConfig({
+      port: 8081,
+      rootDir,
+    });
+
+    ({ server } = await startServer(serverConfig));
+    browser = await puppeteer.launch();
+    rimraf.sync(rootDir);
+  });
+
+  after(async () => {
+    await browser.close();
+    await new Promise(resolve =>
+      server.close(() => {
+        resolve();
+      }),
+    );
+  });
+
+  testCases.forEach(testCase => {
+    ['modern', 'modern-and-legacy'].forEach(mode => {
+      describe(`testcase ${testCase}-${mode}`, function describe() {
+        this.timeout(10000);
+        let page;
+
+        before(async () => {
+          const configName = mode === 'modern' ? 'rollup.modern.config.js' : 'rollup.config.js';
+          const configPath = path.join(__dirname, '..', 'demo', testCase, configName);
+          const config = require(configPath);
+
+          if (Array.isArray(config)) {
+            const bundles = await Promise.all([rollup(config[0]), rollup(config[1])]);
+            await bundles[0].write(config[0].output);
+            await bundles[1].write(config[1].output);
+          } else {
+            const bundle = await rollup(config);
+            await bundle.write(config.output);
+          }
+
+          page = await browser.newPage();
+          await page.goto('http://localhost:8081/', {
+            waitUntil: 'networkidle0',
+          });
+        });
+
+        after(() => {
+          rimraf.sync(rootDir);
+        });
+
+        it('passes the in-browser tests', async () => {
+          // @ts-ignore
+          const browserTests = await page.evaluate(() => window.__tests);
+          expect(browserTests).to.eql({
+            partialCSS: true,
+            litElement: true,
+            startsWith: true,
+            map: true,
+            importMeta: true,
+            importMeta2: true,
+            asyncFunction: true,
+            forOf: true,
+          });
+        });
+
+        it('outputs a service worker', () => {
+          expect(fs.existsSync(path.join(rootDir, 'sw.js'))).to.be.true;
+        });
+      });
+    });
+  });
+});
