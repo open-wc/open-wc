77a38c757dc2c02e5d43265aa211785fb65e9e46
Lars den Bakker <larsdenbakker@gmail.com>
2019-11-28 23:30:29 +0100

    feat(demoing-storybook): add storybook-start help command

--- packages/demoing-storybook/src/start/readCommandLineArgs.js
+++ packages/demoing-storybook/src/start/readCommandLineArgs.js
@@ -1,8 +1,12 @@
 const commandLineArgs = require('command-line-args');
-const { readCommandLineArgs: esDevServerCommandLineArgs } = require('es-dev-server');
+const commandLineUsage = require('command-line-usage');
+const {
+  readCommandLineArgs: esDevServerCommandLineArgs,
+  commandLineOptions: esDevServerCliOptions,
+} = require('es-dev-server');
 
 module.exports = function readCommandLineArgs() {
-  const optionDefinitions = [
+  const cliOptions = [
     {
       name: 'config-dir',
       alias: 'd',
@@ -14,11 +18,39 @@ module.exports = function readCommandLineArgs() {
       name: 'stories',
       alias: 's',
       defaultValue: './stories/*.stories.{js,mdx}',
-      description: 'List of story files e.g. --stories stories/*.stories.{js,mdx}',
+      description: 'List of story files e.g. --stories stories/*.stories.\\{js,mdx\\}',
     },
+    { name: 'help', type: Boolean, description: 'See all options' },
   ];
 
-  const storybookServerConfig = commandLineArgs(optionDefinitions, { partial: true });
+  const storybookServerConfig = commandLineArgs(cliOptions, { partial: true });
+
+  if ('help' in storybookServerConfig) {
+    /* eslint-disable-next-line no-console */
+    console.log(
+      commandLineUsage([
+        {
+          header: 'storybook-start',
+          content: `
+          Storybook start command. Wraps the es-dev-server, adding storybook specific functionality. All regular es-dev-server commands are available.
+
+          Usage: \`storybook-start [options...]\`
+        `.trim(),
+        },
+        {
+          header: 'storybook options',
+          optionList: cliOptions,
+        },
+        {
+          header: 'es-dev-server options',
+          content: '',
+          optionList: esDevServerCliOptions,
+        },
+      ]),
+    );
+    process.exit();
+  }
+
   const esDevServerConfig = esDevServerCommandLineArgs(storybookServerConfig._unknown || []);
 
   return { storybookServerConfig, esDevServerConfig };
