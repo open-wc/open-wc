5b8c2407d19714445adeabf08c33ba1c520de343
Thomas Allmer <d4kmor@gmail.com>
2019-12-10 16:41:01 +0100

    feat: add new package storybook-addon-web-components-knobs

---
+++ packages/storybook-addon-web-components-knobs/index.js
@@ -0,0 +1,33 @@
+export { html } from 'lit-html';
+
+export {
+  storiesOf,
+  addParameters,
+  addDecorator,
+  setCustomElements,
+  getCustomElements,
+  isValidComponent,
+  isValidMetaData,
+  configure,
+} from '@storybook/web-components';
+export { action } from '@storybook/addon-actions';
+export { withA11y } from '@storybook/addon-a11y';
+export { linkTo } from '@storybook/addon-links';
+export { document } from 'global';
+export {
+  withKnobs,
+  text,
+  button,
+  number,
+  select,
+  date,
+  object,
+  color,
+  array,
+  boolean,
+  radios,
+  files,
+  optionsKnob,
+} from '@storybook/addon-knobs';
+
+export { withWebComponentsKnobs } from './src/withWebComponentsKnobs.js';
