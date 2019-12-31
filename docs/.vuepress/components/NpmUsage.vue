f10e938ed9afbe7d95d878e6b676d4f1c5c179c3
Passle <pascal.schilp@ing.com>
2019-12-31 09:57:27 +0100

    docs: add frequency of npms.io measurement

--- docs/.vuepress/components/NpmUsage.vue
+++ docs/.vuepress/components/NpmUsage.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <div v-if="loading">
+    <p v-if="loading">
       Loading...
-    </div>
+    </p>
 
-    <div v-if="rejected">
-      <p>Unable to get usage statistics for {{ routerName }}</p>
-    </div>
+    <p v-if="rejected">
+      Unable to get usage statistics for {{ routerName }}
+    </p>
 
     <div v-if="success">
       <p>
-        <b>Usage stats:</b> {{ usageCount }} downloads (<a target="_blank" :href="'https://www.npmtrends.com/' + routerName">see more</a>)
+        <b>Usage stats:</b> {{ usageCount }} downloads (measured every 15 days) <a target="_blank" :href="'https://www.npmtrends.com/' + routerName">see more</a>
       </p>
       <p>
         <b>Github stars:</b> {{ stars }}
