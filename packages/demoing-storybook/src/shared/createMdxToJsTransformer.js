b640205c26d9f07d785c122ef69b81f7584e456a
Lars den Bakker <larsdenbakker@gmail.com>
2020-01-04 00:30:20 +0100

    fix(demoing-storybook): configure stories from dynamic preview path

--- packages/demoing-storybook/src/shared/createMdxToJsTransformer.js
+++ packages/demoing-storybook/src/shared/createMdxToJsTransformer.js
@@ -1,15 +1,10 @@
 const mdx = require('@mdx-js/mdx');
-const path = require('path');
 const { transformAsync } = require('@babel/core');
 const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
-const toBrowserPath = require('./toBrowserPath');
 
 const compilers = [createCompiler({})];
 
-module.exports = function createMdxToJsTransformer({ rootDir, previewPath }) {
-  const previewPathRelative = rootDir ? `/${path.relative(rootDir, previewPath)}` : previewPath;
-  const previewImport = toBrowserPath(previewPathRelative);
-
+module.exports = function createMdxToJsTransformer({ previewImport }) {
   return async function transformMdxToJs(filePath, body) {
     const jsx = `
       import { React, mdx } from '${previewImport}';
