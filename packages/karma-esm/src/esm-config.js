ea69d3adeba6405c5712685c3d4171cf79d9aecf
Lars den Bakker <larsdenbakker@gmail.com>
2020-01-18 21:06:53 +0100

    feat(karma-esm): use new polyfills loader

--- packages/karma-esm/src/esm-config.js
+++ packages/karma-esm/src/esm-config.js
@@ -17,7 +17,7 @@ const deepmerge = require('deepmerge');
  * @property {string[]} [babelModernExclude]
  * @property {string[]} [babelModuleExclude]
  * @property {boolean} [preserveSymlinks]
- * @property {Partial<import('@open-wc/building-utils/index-html/create-index-html').PolyfillsConfig>} polyfills
+ * @property {import('./utils/inject-polyfills-loader-types').PolyfillsLoaderConfig} [polyfillsLoader]
  */
 
 function createEsmConfig(karmaConfig) {
