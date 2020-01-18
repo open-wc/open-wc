7d3f0c437b1f6f8e124af8243102233466e14200
Lars den Bakker <larsdenbakker@gmail.com>
2020-01-18 21:06:05 +0100

    feat(polyfills-loader): add polyfills loader

---
+++ packages/polyfills-loader/src/inject-polyfills-loader.js
@@ -0,0 +1,94 @@
+/** @typedef {import('parse5').Document} DocumentAst */
+/** @typedef {import('./types').PolyfillsLoaderConfig} PolyfillsLoaderConfig */
+/** @typedef {import('./types').GeneratedFile} GeneratedFile */
+
+const { parse, serialize } = require('parse5');
+const {
+  query,
+  predicates,
+  setAttribute,
+  insertAfter,
+  append,
+  cloneNode,
+} = require('@open-wc/building-utils/dom5-fork');
+const { createPolyfillsLoader } = require('./create-polyfills-loader');
+const { createScript, findImportMapScripts, hasFileOfType, fileTypes } = require('./utils');
+
+/**
+ * @param {DocumentAst} headAst
+ * @param {Node} originalScript
+ * @param {string} type
+ */
+function injectImportMapPolyfill(headAst, originalScript, type) {
+  const systemJsScript = cloneNode(originalScript);
+  setAttribute(systemJsScript, 'type', type);
+  insertAfter(headAst, originalScript, systemJsScript);
+}
+
+/**
+ * @param {DocumentAst} documentAst
+ * @param {Node} headAst
+ * @param {PolyfillsLoaderConfig} cfg
+ */
+function injectImportMapPolyfills(documentAst, headAst, cfg) {
+  const importMapScripts = findImportMapScripts(documentAst);
+  if (importMapScripts.external.length === 0 && importMapScripts.inline.length === 0) {
+    return;
+  }
+
+  const polyfillSystemJs = hasFileOfType(cfg, fileTypes.SYSTEMJS);
+  const polyfillEsModuleShims = hasFileOfType(cfg, fileTypes.ES_MODULE_SHIMS);
+
+  const importMaps = /** @type {Node[]} */ ([
+    ...importMapScripts.external,
+    ...importMapScripts.inline,
+  ]);
+  importMaps.forEach(originalScript => {
+    if (polyfillSystemJs) {
+      injectImportMapPolyfill(headAst, originalScript, 'systemjs-importmap');
+    }
+
+    if (polyfillEsModuleShims) {
+      injectImportMapPolyfill(headAst, originalScript, 'importmap-shim');
+    }
+  });
+}
+
+/**
+ * Transforms an index.html file, injecting a polyfills loader for
+ * compatibility with older browsers.
+ *
+ * @param {string} htmlString
+ * @param {PolyfillsLoaderConfig} cfg
+ * @returns {{ htmlString: string, polyfillFiles: GeneratedFile[] }}
+ */
+function injectPolyfillsLoader(htmlString, cfg) {
+  const documentAst = parse(htmlString);
+
+  const headAst = query(documentAst, predicates.hasTagName('head'));
+  const bodyAst = query(documentAst, predicates.hasTagName('body'));
+
+  if (!headAst || !bodyAst) {
+    throw new Error(`Invalid index.html: missing <head> or <body>`);
+  }
+
+  const polyfillsLoader = createPolyfillsLoader(cfg);
+
+  if (polyfillsLoader === null) {
+    return { htmlString, polyfillFiles: [] };
+  }
+
+  const loaderScript = createScript({}, polyfillsLoader.code);
+  append(bodyAst, loaderScript);
+
+  injectImportMapPolyfills(documentAst, headAst, cfg);
+
+  return {
+    htmlString: serialize(documentAst),
+    polyfillFiles: polyfillsLoader.polyfillFiles,
+  };
+}
+
+module.exports = {
+  injectPolyfillsLoader,
+};
---
+++ packages/polyfills-loader/test/snapshots/create-polyfills-loader/system-resource.js
@@ -0,0 +1,41 @@
+(function () {
+  function loadScript(src, type) {
+    return new Promise(function (resolve) {
+      var script = document.createElement('script');
+
+      function onLoaded() {
+        document.head.removeChild(script);
+        resolve();
+      }
+
+      script.src = src;
+      script.onload = onLoaded;
+
+      script.onerror = function () {
+        console.error('[polyfills-loader] failed to load: ' + src + ' check the network tab for HTTP status.');
+        onLoaded();
+      };
+
+      if (type) script.type = type;
+      document.head.appendChild(script);
+    });
+  }
+
+  var polyfills = [];
+
+  if (!('fetch' in window)) {
+    polyfills.push(loadScript('./polyfills/fetch.js'));
+  }
+
+  polyfills.push(loadScript('./polyfills/systemjs.js'));
+
+  function loadFiles() {
+    System.import('./app.js');
+  }
+
+  if (polyfills.length) {
+    Promise.all(polyfills).then(loadFiles);
+  } else {
+    loadFiles();
+  }
+})();
