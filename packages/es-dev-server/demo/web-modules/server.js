df01c3ae87d5eacf3230a1ea364d0afa388d4109
Passle <pascalschilp@gmail.com>
2020-02-08 16:39:38 +0100

    chore: more cleanup

--- packages/es-dev-server/demo/web-modules/server.js
+++ packages/es-dev-server/demo/web-modules/server.js
@@ -1,6 +1,5 @@
 const path = require('path');
-console.log('adfasdfadsfasdf')
-console.log(__dirname);
+
 module.exports = {
   rootDir: path.resolve(__dirname, '../../../../'),
   appIndex: 'packages/es-dev-server/demo/web-modules/index.html',
