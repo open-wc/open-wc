36462a8f9141bd6bb0c9574c8e6b5057379ca541
Benny Powers <web@bennypowers.com>
2020-02-21 12:20:39 +0200

    feat(lit-helpers): add ReadOnlyPropertiesMixin
    ReadOnlyPropertiesMixin allows `readOnly: true` in property
    declarations, and adds
    `setReadOnlyProperties(properties: Object<string, any>): Promise<this>`
    to instances

---
+++ packages/lit-helpers/test/read-only-properties-mixin.test.js
@@ -0,0 +1,39 @@
+import { expect, html, fixture, defineCE, unsafeStatic } from '@open-wc/testing';
+import { LitElement } from 'lit-element';
+import { ReadOnlyPropertiesMixin } from '../src/read-only-properties-mixin.js';
+
+describe('ReadOnlyPropertiesMixin', () => {
+  let element;
+  beforeEach(async () => {
+    const ReadOnlyElement = unsafeStatic(
+      defineCE(
+        class extends ReadOnlyPropertiesMixin(LitElement) {
+          static get properties() {
+            return {
+              publicString: { type: String },
+              readOnlyString: { type: String, readOnly: true },
+            };
+          }
+
+          constructor() {
+            super();
+            this.readOnlyString = 'readOnlyString';
+          }
+        },
+      ),
+    );
+    element = await fixture(html`<${ReadOnlyElement}></${ReadOnlyElement}>`);
+  });
+
+  it('defines a read only property', async () => {
+    const initial = element.readOnlyString;
+    element.readOnlyString = Date.now().toString(36);
+    expect(element.readOnlyString).to.equal(initial);
+  });
+
+  it('exposes `setReadOnlyProperties()` to "privately" set properties', async () => {
+    const privately = Date.now().toString(36);
+    await element.setReadOnlyProperties({ readOnlyString: privately });
+    expect(element.readOnlyString).to.equal(privately);
+  });
+});
