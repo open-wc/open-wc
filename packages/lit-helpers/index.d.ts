703f57d655f6c8016f891731ec2c22abff73d455
Benny Powers <bennypowers@users.noreply.github.com>
2020-02-22 21:21:21 +0200

    fix(lit-helpers): add type declarations for exports (#1363)

---
+++ packages/lit-helpers/index.d.ts
@@ -0,0 +1,4 @@
+export { live } from './src/live';
+export { spread } from './src/spread';
+export { spreadProps } from './src/spreadProps';
+export { ReadOnlyPropertiesMixin } from './src/read-only-properties-mixin';
---
+++ packages/lit-helpers/src/spread.d.ts
@@ -0,0 +1,3 @@
+import { AttributePart } from 'lit-html';
+
+export declare const spread: (spreadData: { [key: string]: unknown }) => (part: AttributePart) => void;
