18690c59acb86885da0546e80d12b4f8b1b3422e
Benny Powers <bennypowers@users.noreply.github.com>
2020-02-22 20:09:18 +0200

    feat(lit-helpers): add ReadOnlyPropertiesMixin (#1265)
    ReadOnlyPropertiesMixin allows `readOnly: true` in property
    declarations, and adds
    `setReadOnlyProperties(properties: Object<string, any>): Promise<this>`
    to instances

--- packages/lit-helpers/index.js
+++ packages/lit-helpers/index.js
@@ -1,3 +1,4 @@
 export { live } from './src/live.js';
 export { spread } from './src/spread.js';
 export { spreadProps } from './src/spreadProps.js';
+export { ReadOnlyPropertiesMixin } from './src/read-only-properties-mixin.js';
---
+++ packages/lit-helpers/src/read-only-properties.d.ts
@@ -0,0 +1,27 @@
+// when we update to typescript 3.8
+// import type { LitElement } from "lit-element";
+import { LitElement } from "lit-element";
+
+declare type Constructor<T = {}> = new (...args: any[]) => T;
+
+declare module 'lit-element/lib/updating-element' {
+  interface PropertyDeclaration {
+    readOnly?: boolean;
+  }
+}
+
+export interface ReadOnlyClass {
+  /**
+   * Set read-only properties
+   */
+  setReadOnlyProperties(props: {
+    [s: string]: unknown;
+  }): Promise<void>;
+}
+
+/**
+ * Enables the nofity option for properties to fire change notification events
+ */
+export declare function ReadOnlyPropertiesMixin<T extends Constructor<LitElement>>(superclass: T): T & Constructor<ReadOnlyClass>;
+
+export {};
