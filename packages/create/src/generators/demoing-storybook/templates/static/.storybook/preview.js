f92509fe0a31b7e8526e9d3c6325aa3567cc11ed
Lars den Bakker <larsdenbakker@gmail.com>
2020-03-01 19:42:56 +0100

    fix(create): fix race condition with registering addons

--- packages/create/src/generators/demoing-storybook/templates/static/.storybook/preview.js
+++ packages/create/src/generators/demoing-storybook/templates/static/.storybook/preview.js
@@ -5,26 +5,27 @@ import {
   withA11y
 } from '@open-wc/demoing-storybook';
 
+addDecorator(withA11y);
+
+addParameters({
+  a11y: {
+    config: {},
+    options: {
+      checks: { 'color-contrast': { options: { noScroll: true } } },
+      restoreScroll: true
+    }
+  },
+  docs: {
+    iframeHeight: '200px'
+  }
+});
+
 async function run() {
   const customElements = await (
     await fetch(new URL('../custom-elements.json', import.meta.url))
   ).json();
+
   setCustomElements(customElements);
-
-  addDecorator(withA11y);
-
-  addParameters({
-    a11y: {
-      config: {},
-      options: {
-        checks: { 'color-contrast': { options: { noScroll: true } } },
-        restoreScroll: true
-      }
-    },
-    docs: {
-      iframeHeight: '200px'
-    }
-  });
 }
 
 run();
