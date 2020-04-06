a895e3ab2f5dc57026ff55af04c8909969bdc641
Thomas Allmer <d4kmor@gmail.com>
2020-04-06 12:15:46 +0200

    fix(testing): auto load chai plugin typings

--- packages/testing/register-chai-plugins.d.ts
+++ packages/testing/register-chai-plugins.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="@open-wc/semantic-dom-diff" />
 /// <reference types="chai-a11y-axe" />
+/// <reference types="@types/chai-dom" />
+/// <reference types="@types/sinon-chai" />
 
 export {};
