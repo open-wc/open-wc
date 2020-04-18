79f9558d11ca115e1fd7bb2c381d26262fccf2b0
Lars den Bakker <larsdenbakker@gmail.com>
2020-04-18 18:29:16 +0200

    fix(demoing-storybook): ensure regenerator runtime is always loaded

--- packages/demoing-storybook/src/build/rollup/createRollupConfig.js
+++ packages/demoing-storybook/src/build/rollup/createRollupConfig.js
@@ -96,7 +96,7 @@ function createRollupConfig({ outputDir, indexFilename, indexHTMLString }) {
           coreJs: true,
           fetch: true,
           abortController: true,
-          regeneratorRuntime: true,
+          regeneratorRuntime: 'always',
           webcomponents: true,
           intersectionObserver: true,
           resizeObserver: true,
