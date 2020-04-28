9d35ca2fb8423e832e130dcf24a6113494c75c80
Abdón Rodríguez Davila <a@abdonrd.com>
2020-04-28 12:51:08 +0200

    Update the Service Worker default config (#1574)
    * fix(building-rollup): update previous legacy to current nomodule

--- packages/building-rollup/src/createSpaConfig.js
+++ packages/building-rollup/src/createSpaConfig.js
@@ -77,14 +77,14 @@ function createSpaConfig(options) {
           generateSW,
           userOptions.workbox,
           {
-            globIgnores: ['legacy-*.js'],
+            globIgnores: ['polyfills/*.js', 'legacy-*.js', 'nomodule-*.js'],
             navigateFallback: '/index.html',
             // where to output the generated sw
             swDest: path.join(process.cwd(), outputDir, 'sw.js'),
             // directory to match patterns against to be precached
             globDirectory: path.join(process.cwd(), outputDir),
             // cache any html js and css by default
-            globPatterns: ['**/*.{html,js,css}'],
+            globPatterns: ['**/*.{html,js,css,webmanifest}'],
             skipWaiting: true,
             clientsClaim: true,
             runtimeCaching: [
