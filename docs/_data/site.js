b459228210f1f530d307fd65fc4a04c1a5bb701e
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-05-09 15:43:57 -0400

    docs: use base tag

--- docs/_data/site.js
+++ docs/_data/site.js
@@ -2,5 +2,5 @@ module.exports = {
   name: 'Open Web Components',
   shortDesc:
     'Open Web Components provides a set of defaults, recommendations and tools to help facilitate your web component project. Our recommendations include: developing, linting, testing, building, tooling, demoing, publishing and automating.',
-  url: process.env.DEPLOY_URL ? process.env.DEPLOY_URL : '/',
+  url: process.env.DEPLOY_URL ? process.env.DEPLOY_URL : '/_site-dev/',
 };
