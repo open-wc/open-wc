5028c6a6d0c532740b429a74ddd7903c0a6238de
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-05-14 07:50:43 -0400

    docs: apply specific type of URL by deploy type

--- docs/_data/site.js
+++ docs/_data/site.js
@@ -1,6 +1,22 @@
+let url = '/';
+
+switch (process.env.CONTEXT) {
+  case 'production':
+    url = process.env.URL;
+    break;
+  case 'deploy-preview':
+    url = process.env.DEPLOY_URL;
+    break;
+  case 'branch-deploy':
+    url = process.env.DEPLOY_PRIME_URL;
+    break;
+  default:
+    break;
+}
+
 module.exports = {
   name: 'Open Web Components',
   shortDesc:
     'Open Web Components provides a set of defaults, recommendations and tools to help facilitate your web component project. Our recommendations include: developing, linting, testing, building, tooling, demoing, publishing and automating.',
-  url: process.env.DEPLOY_URL ? process.env.DEPLOY_URL : '/',
+  url,
 };
