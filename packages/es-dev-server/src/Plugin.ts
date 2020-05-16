0e9a900508980da5ec553934fc6c5f74693bf6aa
Lars den Bakker <larsdenbakker@gmail.com>
2020-05-16 15:52:18 +0200

    fix(es-dev-server): really fix plugin types

--- packages/es-dev-server/src/Plugin.ts
+++ packages/es-dev-server/src/Plugin.ts
@@ -3,9 +3,13 @@ import { FSWatcher } from 'chokidar';
 import { Server } from 'net';
 import { ParsedConfig } from './config';
 
-type ServeResult = void | { body: string; type?: string; headers?: Record<string, string> };
-type TransformResult = void | { body?: string; headers?: Record<string, string> };
-type ResolveResult = void | string | Promise<void> | Promise<string>;
+type ServeResult =
+  | undefined
+  | null
+  | { body: string; type?: string; headers?: Record<string, string> };
+type TransformResult = undefined | null | { body?: string; headers?: Record<string, string> };
+type ResolveResult = undefined | null | string;
+type ResolveMimeTypeResult = undefined | null | string;
 
 interface ServerArgs {
   config: ParsedConfig;
@@ -18,6 +22,9 @@ export interface Plugin {
   serverStart?(args: ServerArgs): void | Promise<void>;
   serve?(context: Context): ServeResult | Promise<ServeResult>;
   transform?(context: Context): TransformResult | Promise<TransformResult>;
-  resolveImport?(args: { source: string; context: Context }): ResolveResult;
-  resolveMimeType?(context: Context): undefined | string | Promise<undefined | string>;
+  resolveImport?(args: {
+    source: string;
+    context: Context;
+  }): ResolveResult | Promise<ResolveResult>;
+  resolveMimeType?(context: Context): ResolveMimeTypeResult | Promise<ResolveMimeTypeResult>;
 }
