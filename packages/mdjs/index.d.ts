c8ea754153e2da4e73fa1f6a927c7b24108bf658
Thomas Allmer <d4kmor@gmail.com>
2020-05-23 09:36:38 +0200

    fix(core): export type for plugins

--- packages/mdjs/index.d.ts
+++ packages/mdjs/index.d.ts
@@ -3,5 +3,5 @@ import { mdjsStoryParse } from './src/mdjsStoryParse';
 import { mdjsDocPage } from './src/mdjsDocPage';
 import { resolveToUnpkg } from './src/resolveToUnpkg';
 
-export { MarkdownResult, ParseResult, Story, ProcessResult } from './src/types';
+export { MarkdownResult, ParseResult, Story, ProcessResult, MdjsProcessPlugin } from './src/types';
 export { mdjsParse, mdjsStoryParse, mdjsDocPage, resolveToUnpkg };
