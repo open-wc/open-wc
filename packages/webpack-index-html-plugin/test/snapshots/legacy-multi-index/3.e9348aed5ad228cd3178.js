e374da2e959573060e5597f827d210c6cd6b47d1
Thomas Allmer <d4kmor@gmail.com>
2020-10-22 08:28:32 +0200

    chore: move webpack tools into the legacy repo

--- packages/webpack-index-html-plugin/test/snapshots/legacy-multi-index/3.e9348aed5ad228cd3178.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],[,function(o,n){console.log("my lazy")}]]);
--- packages/webpack-index-html-plugin/test/snapshots/multi-entry/2.3033b81b362ac32748c1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{2:function(o,n){console.log("my lazy")}}]);
