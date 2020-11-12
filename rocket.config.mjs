22fac813f0caab7cfdcb190c59a03ab5ceb89bc7
Thomas Allmer <d4kmor@gmail.com>
2020-11-12 17:06:22 +0100

    chore: hash all assets

--- rocket.config.mjs
+++ rocket.config.mjs
@@ -1,9 +1,11 @@
 import { rocketLaunch } from '@d4kmor/launch';
 import { rocketSearch } from '@d4kmor/search';
+import { absoluteBaseUrlNetlify } from '@d4kmor/core/helpers';
 
 export default {
   themes: [rocketLaunch(), rocketSearch()],
   build: {
     emptyOutputDir: false,
+    absoluteBaseUrl: absoluteBaseUrlNetlify('http://localhost:8080'),
   },
 };
