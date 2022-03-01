53b28775f135b04d4b6f63fbbf4cf9d0bca04aa5
thepassle <pascalschilp@gmail.com>
2022-03-01 09:46:20 +0100

    fix: add a type for lit v1 renderable

--- packages/testing-helpers/src/renderable.ts
+++ packages/testing-helpers/src/renderable.ts
@@ -1,7 +1,15 @@
 import type { TemplateResult } from 'lit';
 
+interface Lit1TemplateResult { 
+  processor: any,
+  strings: TemplateStringsArray,
+  type: string,
+  values: readonly unknown[]
+}
+
 export type LitHTMLRenderable = (
-    TemplateResult
+    Lit1TemplateResult 
+  | TemplateResult
   | TemplateResult[]
   | Node | Node[]
   | string | string[]
