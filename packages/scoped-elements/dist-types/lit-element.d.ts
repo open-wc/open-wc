82131d993c26e16a4a054ea7eb47a9474b41857c
thepassle <pascalschilp@gmail.com>
2023-11-03 17:09:56 +0100

    fix: scoped elements types

--- packages/scoped-elements/dist-types/lit-element.d.ts
+++ packages/scoped-elements/dist-types/lit-element.d.ts
@@ -1,18 +1,18 @@
 /**
- * @typedef {import('./types').ScopedElementsHost} ScopedElementsHost
- * @typedef {import('./types').ScopedElementsMap} ScopedElementsMap
+ * @typedef {import('./types.js').ScopedElementsHost} ScopedElementsHost
+ * @typedef {import('./types.js').ScopedElementsMap} ScopedElementsMap
  * @typedef {import('lit').CSSResultOrNative} CSSResultOrNative
  * @typedef {import('lit').LitElement} LitElement
  * @typedef {typeof import('lit').LitElement} TypeofLitElement
  */
 /**
- * @template {import('./types').Constructor<LitElement>} T
+ * @template {import('./types.js').Constructor<LitElement>} T
  * @param {T} superclass
- * @return {T & import('./types').Constructor<ScopedElementsHost>}
+ * @return {T & import('./types.js').Constructor<ScopedElementsHost>}
  */
-export function ScopedElementsMixin<T extends import("@open-wc/dedupe-mixin").Constructor<import("lit").LitElement>>(superclass: T): T & import("@open-wc/dedupe-mixin").Constructor<import("./types").ScopedElementsHost>;
-export type ScopedElementsHost = import('./types').ScopedElementsHost;
-export type ScopedElementsMap = import('./types').ScopedElementsMap;
+export function ScopedElementsMixin<T extends import("@open-wc/dedupe-mixin").Constructor<import("lit").LitElement>>(superclass: T): T & import("@open-wc/dedupe-mixin").Constructor<import("./types.js").ScopedElementsHost>;
+export type ScopedElementsHost = import('./types.js').ScopedElementsHost;
+export type ScopedElementsMap = import('./types.js').ScopedElementsMap;
 export type CSSResultOrNative = import('lit').CSSResultOrNative;
 export type LitElement = import('lit').LitElement;
 export type TypeofLitElement = typeof import('lit').LitElement;
