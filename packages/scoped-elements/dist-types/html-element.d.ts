3e6fff71abf04d35374e9ba0902c167f7de2e5ce
Pascal Schilp <pascalschilp@gmail.com>
2023-11-22 16:38:23 +0100

    fix: fix scoped element types for angular (#2751)
    * fix: fix scoped element types for angular
    
    * chore: changeset

--- packages/scoped-elements/dist-types/html-element.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @template {import('./types.js').Constructor<HTMLElement>} T
- * @param {T} superclass
- * @return {T & import('./types.js').Constructor<ScopedElementsHost>}
- */
-export function ScopedElementsMixin<T extends import("@open-wc/dedupe-mixin").Constructor<HTMLElement>>(superclass: T): T & import("@open-wc/dedupe-mixin").Constructor<import("./types.js").ScopedElementsHost>;
-export type ScopedElementsHost = import('./types.js').ScopedElementsHost;
-export type ScopedElementsMap = import('./types.js').ScopedElementsMap;
-//# sourceMappingURL=html-element.d.ts.map
