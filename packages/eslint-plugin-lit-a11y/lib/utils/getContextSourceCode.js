ff1c567cbd556b6145a80d1cb4686914351d672d
Nikhil Hundare <hundare@gmail.com>
2024-01-17 13:10:51 +0100

    added more description to the method

--- packages/eslint-plugin-lit-a11y/lib/utils/getContextSourceCode.js
+++ packages/eslint-plugin-lit-a11y/lib/utils/getContextSourceCode.js
@@ -1,6 +1,8 @@
 /**
- * Returns checks backward compatiblity and returns context.sourceCode
- *
+ * The new eslint v9 alpha version which has breaking changes
+ * This version has deprecated context.getSourceCode() instead context.sourceCode should be used
+ * The getContextSourceCode() checks backward compatiblity and returns context.sourceCode
+ * This method can be removed once v9 is released
  */
 function getContextSourceCode(context) {
   try {
