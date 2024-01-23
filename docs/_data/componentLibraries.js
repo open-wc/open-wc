078ba3ff798d76ab7062a61d67f7ba9e03f2f655
Michael Worm <3817545+michaelworm@users.noreply.github.com>
2024-01-23 15:16:42 +0100

    Add Porsche Design System to componentLibraries

--- docs/_data/componentLibraries.js
+++ docs/_data/componentLibraries.js
@@ -136,6 +136,11 @@ const componentLibraries = [
     description: "Red Hat's set of community-created web components based on PatternFly design.",
   },
   {
+    name: 'Porsche Design System',
+    url: 'https://designsystem.porsche.com/',
+    description: "The Porsche Design System provides the design fundamentals and elements for efficiently creating aesthetic and high-quality web applications, including easy-to-use Figma and UX Pin libraries, coded Web Components and comprehensive usage guidelines. Everything is built and tested following the Porsche quality standards and corporate design principles.",
+  },
+  {
     name: 'Siemenx IX',
     url: 'https://ix.siemens.io/',
     description:
