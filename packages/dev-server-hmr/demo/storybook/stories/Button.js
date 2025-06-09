4fb007ded8098397c82b299b3451464fcf1ed229
Pascal Schilp <pascalschilp@gmail.com>
2025-06-09 17:15:07 +0200

    chore: make eslint plugin bundleable and update prettier (#2846)
    fix: prettier (#2847)
    
    chore: package lock

--- packages/dev-server-hmr/demo/storybook/stories/Button.js
+++ packages/dev-server-hmr/demo/storybook/stories/Button.js
@@ -1,7 +1,6 @@
 import { html } from 'lit';
 import '../src/my-button.js';
 
-export const Button = args =>
-  html`
-    <my-button .primary=${args.primary} .size=${args.size || 'medium'}>${args.label}</my-button>
-  `;
+export const Button = args => html`
+  <my-button .primary=${args.primary} .size=${args.size || 'medium'}>${args.label}</my-button>
+`;
